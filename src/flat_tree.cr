18844861ec974a616013e053fe09233437dcbc85
Anh (Duke) Nguyen <58082199+dukeraphaelng@users.noreply.github.com>
2021-04-08 22:39:09 +1000

    docs: add inline comments to methods

--- src/flat_tree.cr
+++ src/flat_tree.cr
@@ -13,6 +13,9 @@ require "./iterator"
 module FlatTree
   extend self
 
+  # Returns a list of all the full roots (subtrees where all nodes have either 2 or 0 children) < index.
+  # For example full_roots(8) returns [3] since the subtree rooted at 3 spans 0 -> 6 and the tree rooted
+  # at 7 has a child located at 9 which is >= 8.
   def full_roots(i : UInt64, nodes : Array(UInt64) = [] of UInt64)
     raise ArgumentError.new("You can only look up roots for depth 0 blocks, got index #{i}") unless i.even?
 
@@ -34,21 +37,25 @@ module FlatTree
     nodes
   end
 
+  # Returns the depth of an element
   def depth(i : UInt64?) : UInt64
     return 0_u64 if i.nil?
     (~BigInt.new(i)).trailing_zeros_count.to_u64
   end
 
+  # Returns the index of this elements sibling
   def sibling(i : UInt64, d : UInt64? = nil)
     d = self.depth(i) if d.nil?
     self.index(d, self.offset(i, d) ^ 1)
   end
 
+  # Returns the index of the parent element in tree
   def parent(i : UInt64, d : UInt64? = nil) : UInt64
     d = self.depth(i) if d.nil?
     self.index(d + 1_u64, self.offset(i, d) >> 1_u64)
   end
 
+  # Returns only the left child of a node.
   def left_child(i : UInt64 | Nil, d : UInt64? = nil) : UInt64 | Nil
     return nil if i.nil? || i.as(UInt64).even?
     i = i.as(UInt64)
@@ -60,6 +67,7 @@ module FlatTree
     end
   end
 
+  # Returns only the right child of a node.
   def right_child(i : UInt64 | Nil, d : UInt64? = nil) : UInt64 | Nil
     return nil if i.nil? || i.as(UInt64).even?
     d = self.depth(i) if d.nil?
@@ -70,6 +78,8 @@ module FlatTree
     end
   end
 
+  # Returns an array [left_child, right_child] with the indexes of this elements children.
+  # If this element does not have any children it returns null
   def children(i : UInt64, d : UInt64? = nil) : Array(UInt64) | Nil
     return nil if i.even?
 
@@ -82,23 +92,28 @@ module FlatTree
     ]
   end
 
+  # Returns the left spanning in index in the tree index spans.
   def left_span(i : UInt64, d : UInt64? = nil)
     return i if i == 0
     d = self.depth(i) if d.nil?
     offset(i, d) * (2_u64 << d)
   end
 
+  # Returns the right spanning in index in the tree index spans.
   def right_span(i : UInt64, d : UInt64? = nil)
     return i if i.even?
     d = self.depth(i) if d.nil?
     (offset(i, d) + 1_u64) * (2_u64 << d) - 2_u64
   end
 
+  # Returns how many nodes (including parent nodes) a tree contains
   def count(i : UInt64, d : UInt64? = nil)
     d = self.depth(i) if d.nil?
     (2 << d) - 1_u64
   end
 
+  # Returns the range (inclusive) the tree root at index spans.
+  # For example FlatTree.spans(3) would return [0, 6] (see the usage example).
   def spans(i : UInt64, d : UInt64? = nil)
     return [i, i] if i.even?
     d = self.depth(i) if d.nil?
@@ -108,16 +123,19 @@ module FlatTree
     [o * width, (o + 1_u64) * width - 2_u64]
   end
 
+  # Returns an array index for the tree element at the given depth and offset
   def index(d : UInt64, o : UInt64) : UInt64
     ((1_u64 + 2_u64 * o) * two_pow(d) - 1_u64).to_u64
   end
 
+  # Returns the relative offset of an element
   def offset(i : UInt64, d : UInt64? = nil) : UInt64
     return (i/2_u64).to_u64 if i.even?
     d = self.depth(i) if d.nil?
     i >> (d + 1)
   end
 
+  # Create a stateful tree iterator starting at a given index. The iterator exposes the following methods.
   def iterator(i : UInt64 = 0u64)
     Iterator.new(i)
   end
--- src/iterator.cr
+++ src/iterator.cr
@@ -8,6 +8,7 @@ class FlatTree::Iterator
     self
   end
 
+  # Move the iterator the this specific tree index.
   def seek(i : UInt64)
     @index = i
     @offset, @factor = if (@index & 1_u64)
@@ -17,10 +18,12 @@ class FlatTree::Iterator
                        end
   end
 
+  # Is the iterator at a left sibling?
   def is_left
     @offset.even?
   end
 
+  # Is the iterator at a right sibling?
   def is_right
     @offset.odd?
   end
@@ -30,6 +33,7 @@ class FlatTree::Iterator
     condition ? i > (@index - @factor / 2_u64) : true
   end
 
+  # Move the iterator the prev item in the tree.
   def prev
     return @index if @offset == 0
     @offset -= 1_u64
@@ -37,16 +41,19 @@ class FlatTree::Iterator
     @index
   end
 
+  # Move the iterator the next item in the tree.
   def next
     @offset += 1_u64
     @index += @factor
     @index
   end
 
+  # Move the iterator to the current sibling
   def sibling
     @is_left ? self.next : self.prev
   end
 
+  # Move the iterator to the current parent index
   def parent
     if @offset.odd?
       @index -= (@factor/2_u64).to_u64
@@ -59,6 +66,7 @@ class FlatTree::Iterator
     @index
   end
 
+  # Move the iterator to the current left span index.
   def left_span
     @index = @index - (@factor/2_u64).to_u64 + 1_u64
     @offset = (@index/2_u64).to_u64
@@ -66,6 +74,7 @@ class FlatTree::Iterator
     @index
   end
 
+  # Move the iterator to the current right span index.
   def right_span
     @index += @factor/2_u64 - 1_u64
     @offset = (@index/2_u64).to_u64
@@ -73,6 +82,7 @@ class FlatTree::Iterator
     @index
   end
 
+  # Move the iterator to the current left child index.
   def left_child
     return @index if @factor == 2_u64
     @factor = (@factor/2_u64).to_u64
@@ -81,6 +91,7 @@ class FlatTree::Iterator
     @index
   end
 
+  # Move the iterator to the current right child index.
   def right_child
     return @index if @factor == 2_u64
     @factor = (@factor/2_u64).to_u64
